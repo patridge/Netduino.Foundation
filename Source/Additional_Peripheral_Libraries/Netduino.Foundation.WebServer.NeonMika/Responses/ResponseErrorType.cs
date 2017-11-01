9af541d7bd54d9275cc637167d33b62058062ccd
Nevyn <mark@thepcsite.co.uk>
2017-11-01 19:35:25 +0000

    Added source files for NeonMika web server.

---
+++ Source/Additional_Peripheral_Libraries/Netduino.Foundation.WebServer.NeonMika/Responses/ResponseErrorType.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Netduino.Foundation.WebServer.NeonMika.Responses
+{
+    public enum ResponseErrorType 
+    {
+        ParameterMissing,
+        ParameterConvertError,
+        ParameterRangeException,
+        InternalValueNotSet,
+        InternalOperationError
+    }
+}
